0999d0b88a09ed8cab6a16844fdc01aaa4ae9b8a
Daniel Ledwoń <daniel.ledwon@wp.pl>
2020-03-18 12:58:48 +0100

    add new product

--- app/src/main/java/com/example/pazigprojkotlin/services/ProductManager.kt
+++ app/src/main/java/com/example/pazigprojkotlin/services/ProductManager.kt
@@ -4,13 +4,20 @@ import com.example.pazigprojkotlin.model.Product
 
 interface ProductManager {
     fun getProducts(): MutableList<Product>
+    fun addProduct(product: Product)
 }
 
 class ProductManagerImpl() : ProductManager {
+    private val _products : MutableList<Product> = mutableListOf(
+        Product("makaron", 10.5),
+        Product("kasza", 3.2)
+    )
+
     override fun getProducts(): MutableList<Product> {
-        return mutableListOf(
-            Product("makaron", 10.5),
-            Product("kasza", 3.2)
-        )
+        return _products
+    }
+
+    override fun addProduct(product: Product) {
+         _products.add(product)
     }
 }
--- app/src/main/java/com/example/pazigprojkotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/pazigprojkotlin/viewmodel/MainViewModel.kt
@@ -7,4 +7,16 @@ import com.example.pazigprojkotlin.services.ProductManager
 class MainViewModel(private val _productManager: ProductManager) : ViewModel() {
     val testText = MutableLiveData<String>("testText")
     val productAdapter: ProductAdapter = ProductAdapter(_productManager.getProducts())
+
+    val newProductName = MutableLiveData<String>()
+    val newProductPrice = MutableLiveData<String>()
+
+    fun addProduct(){
+        var newProduct = Product(
+            newProductName.value!!,
+            newProductPrice.value!!.toDouble()
+        )
+        _productManager.addProduct(newProduct)
+        productAdapter.addItem(newProduct)
+    }
 }
