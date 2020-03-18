524b4c4244454655c761ee7b98339e8551f891c3
Daniel Ledwoń <daniel.ledwon@wp.pl>
2020-03-18 18:18:05 +0100

    change viewmodel field name - conflict with viewModel() method

--- app/src/main/java/com/example/pazigprojkotlin/MainActivity.kt
+++ app/src/main/java/com/example/pazigprojkotlin/MainActivity.kt
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import org.koin.android.viewmodel.ext.android.viewModel
 import com.example.pazigprojkotlin.databinding.ActivityMainBinding
-import com.example.pazigprojkotlin.model.Product
 import com.example.pazigprojkotlin.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModel()
+    private val _vm: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         // Assign the component to a property in the binding class.
         binding.lifecycleOwner = this
-        binding.viewmodel = viewModel
+        binding.viewmodel = _vm
 
         rvProducts.layoutManager = LinearLayoutManager(this)
-        rvProducts.adapter = viewModel.productAdapter
+        rvProducts.adapter = _vm.productAdapter
     }
 }
